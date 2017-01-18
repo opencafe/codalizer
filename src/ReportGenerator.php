b74c18eaa07b0504e127b0ce0b21e07f9d55e579
Alireza <a.josheghani@anetwork.ir>
2017-01-18 10:58:21 +0330

    Update ReportGenerator Helper object

--- src/ReportGenerator.php
+++ src/ReportGenerator.php
@@ -7,24 +7,37 @@ class ReportGenerator {
     /**
      * Make the report from project
      *
-     * @param $projectPath
-     * @return array
+     * @param $path
+     * @return bool
      */
     public static function make($path)
     {
-        exec("rm report.xml; vendor/bin/phpmd $path xml codesize,unusedcode,naming >> report.xml");
+        if(file_exists("report.xml")){
+            unlink(base_path("report.xml"));
+        }
 
+        if(file_exists(base_path("vendor/bin/phpmd"))){
+            exec("vendor/bin/phpmd $path xml codesize,unusedcode,naming >> report.xml", $output);
+        } else {
+            return false;
+        }
+
+        // load xml report file
         $xml = simplexml_load_file('report.xml');
 
+        // parse the xml report file
         $result = ReportParser::make($xml);
-
         $xml->asXML('report.xml');
 
-        $result['fileViolations']  = count($xml->children());
+        // set total fileViolations
+        $result['fileViolations'] = count($xml->children());
 
-        file_put_contents('FormBuilder.json', json_encode($result) );
+        // generate report as json file
+        if(file_put_contents('FormBuilder.json', json_encode($result) )){
+            return true;
+        }
 
-        return $result;
+        return false;
     }
 
 }
